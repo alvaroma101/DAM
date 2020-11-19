afe6b08d02d0ccc7bc351969a761d97ac5ddad23
Álvaro Méndez Alves <37836114+alvaroma101@users.noreply.github.com>
2020-11-19 18:46:11 +0100

    Inicio

---
+++ PROG/Unidad1/Tarea1/PROG01_TAREA01_mendez_alves_alvaro/Tarea01Ejercicio2.java
@@ -0,0 +1,14 @@
+class Tarea01Ejercicio2 {
+	
+	public static void main(String[] args) {
+		System.out.println("IES AUGUSTOBRIGA"); 
+		System.out.println("M\u00f3dulo Profesional - PROGRAMACI\u00d3N"); 
+		System.out.println("TAREA 01 Introducci\u00f3n a la programaci\u00f3n"); 
+		System.out.println(""); 
+		System.out.println("Ejercicio 2 Trabajando con JAVA SE"); 
+		System.out.println(""); 
+		System.out.println("\u00c1lvaro M\u00e9ndez Alves"); 
+		System.out.println("Alburquerque - Badajoz"); 
+	}
+	
+}
